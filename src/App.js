907e5216b319c1e2c010d15d33cdef33fabf9846
Agathe Cazals <agathe.cazals@gmail.com>
2019-04-08 15:21:18 +0200

    Quete Finie

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, NavLink } from "react-router-dom";
+import Home from './Home';
+import History from './History';
 
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <BrowserRouter>
+        <div>
+          <ul>
+            <li><NavLink to="/"> Accueil </NavLink></li>
+            <li><NavLink to="/notre-histoire" activeStyle={
+              { color: 'red' }}> Histoire  </NavLink></li>
+          </ul>
+          <Route path="/" exact component={Home} />
+          <Route path="/notre-histoire" exact component={History} />
+        </div>
+      </BrowserRouter >
     );
   }
 }
