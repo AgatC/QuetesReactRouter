02b000f79bab8b133445e04fd4c4e070b1a9f7bb
Agathe Cazals <agathe.cazals@gmail.com>
2019-04-08 15:19:41 +0200

    Quete finie

---
+++ src/History.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class History extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Notre histoire</h3>
+      </div>
+    );
+  }
+}
+
+export default History;
